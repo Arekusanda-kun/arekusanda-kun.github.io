e828c5c7f6fbda8e8f72a987f61dfc0e10979322
Arekusanda-kun <34605739+Arekusanda-kun@users.noreply.github.com>
2018-02-06 18:51:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+$(document).ready(function() {
+    $('.menu').click(function() {
+        $('nav ul').slideToggle(500);
+    }); //end slide toggle
+
+    $(window).resize(function() {
+        if ($(window).width() > 500) {
+            $('nav ul').removeAttr('style');
+        }
+    }); //end resize
+}); //end ready
+*/
